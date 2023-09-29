4ca19f64c8ee90601e04bdb2e3bea5db72ce41a3
Darien Salazar <darien.axell.salazar@gmail.com>
2023-09-28 22:57:45 -0500

    Added error message.

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,8 +1,9 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
   const amountInputRef = useRef();
 
   const submitHandler = (e) => {
@@ -16,6 +17,7 @@ const MealItemForm = (props) => {
       enteredAmount < 1 ||
       enteredAmount > 5
     ) {
+      setAmountIsValid(false);
       return;
     }
   };
@@ -35,6 +37,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button className={classes.button}>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
